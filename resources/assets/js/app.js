2f72b8d812b41551ce9da24368d80c605ebb8e33
风声猎猎 <tianya3232@126.com>
2018-09-19 17:09:41 +0800

    first commit

---
+++ resources/assets/js/app.js
@@ -0,0 +1,8 @@
+require('../../../node_modules/jquery/dist/jquery.min')
+require('../../../node_modules/bootstrap/dist/js/bootstrap.min')
+
+require('./move-top')
+require('./easing')
+require('./jquery.flexslider')
+require('./minicart')
+require('./site')
